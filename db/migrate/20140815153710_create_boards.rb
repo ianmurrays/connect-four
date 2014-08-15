5497165b3f2fe999d32aa7cb170b0e3a28850a99
Ian Murray <ianmurrays@gmail.com>
2014-08-15 14:56:45 -0400

    Converts bitboard to bigint

--- db/migrate/20140815153710_create_boards.rb
+++ db/migrate/20140815153710_create_boards.rb
@@ -2,7 +2,7 @@ class CreateBoards < ActiveRecord::Migration
   def change
     create_table :boards do |t|
       t.integer :user_id
-      t.integer :bitboard
+      t.bigint :bitboard
 
       t.timestamps
     end
